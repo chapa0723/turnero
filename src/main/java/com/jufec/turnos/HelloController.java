64ff08af340812c4f08bfbaa531cbe27f0c19545
Chapa <72357747+chapa0723@users.noreply.github.com>
2022-03-21 18:33:36 -0300

    Add files via upload

---
+++ src/main/java/com/jufec/turnos/HelloController.java
@@ -0,0 +1,14 @@
+package com.jufec.turnos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Hola samanta este es un mensaje de prueba");
+    }
+}
